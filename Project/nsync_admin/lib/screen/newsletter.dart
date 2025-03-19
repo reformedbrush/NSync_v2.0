90b4e3b7362e56a4dac17645bc587fb18f4d0a87
Akshai R Sangeeth <akshayrs865@gmail.com>
2025-03-19 14:55:02 +0530

    newsletter file upload

--- Project/nsync_admin/lib/screen/newsletter.dart
+++ Project/nsync_admin/lib/screen/newsletter.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:nsync_admin/components/insert_form.dart';
 import 'package:nsync_admin/main.dart';
@@ -30,19 +34,23 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
       String Title = _newsTitleController.text;
       String Author = _newsAuthorController.text;
       String Content = _newsContentController.text;
+      String? url = await photoUpload();
 
       await supabase.from('tbl_newsletter').insert({
         'newsletter_title': Title,
         'newsletter_author': Author,
         'newsletter_content': Content,
+        'newsletter_image': url,
       });
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text(
-          "NewsLetter Inserted Sucessfully",
-          style: TextStyle(color: Colors.white),
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            "NewsLetter Inserted Sucessfully",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.green,
         ),
-        backgroundColor: Colors.green,
-      ));
+      );
       _newsAuthorController.clear();
       _newsContentController.clear();
       _newsTitleController.clear();
@@ -56,9 +64,9 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
 
   Future<void> fetchNews() async {
     try {
-      final respone = await supabase.from('tbl_newsletter').select();
+      final response = await supabase.from('tbl_newsletter').select();
       setState(() {
-        newsList = respone;
+        newsList = response;
       });
     } catch (e) {
       print("ERROR FETCHING NEWSLETTER DATA: $e");
@@ -71,11 +79,14 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
 
   Future<void> editNews() async {
     try {
-      await supabase.from('tbl_newsletter').update({
-        'newsletter_title': _newsTitleController.text,
-        'newsletter_content': _newsContentController.text,
-        'newsletter_author': _newsAuthorController.text
-      }).eq('id', eid);
+      await supabase
+          .from('tbl_newsletter')
+          .update({
+            'newsletter_title': _newsTitleController.text,
+            'newsletter_content': _newsContentController.text,
+            'newsletter_author': _newsAuthorController.text,
+          })
+          .eq('id', eid);
       fetchNews();
       _newsAuthorController.clear();
       _newsContentController.clear();
@@ -93,6 +104,39 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
       fetchNews();
     } catch (e) {
       print("ERROR DELETING NEWS: $e");
+    }
+  }
+
+  //file upload
+
+  // file upload
+
+  PlatformFile? pickedImage;
+
+  Future<void> handleImagePick() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+    );
+    if (result != null) {
+      setState(() {
+        pickedImage = result.files.first;
+      });
+    }
+  }
+
+  Future<String?> photoUpload() async {
+    if (pickedImage == null) return null; // Prevent errors
+    try {
+      final bucketName = 'Newsletter';
+      final filePath =
+          "News-${pickedImage!.name}-${DateTime.now().millisecondsSinceEpoch}";
+      await supabase.storage
+          .from(bucketName)
+          .uploadBinary(filePath, pickedImage!.bytes!);
+      return supabase.storage.from(bucketName).getPublicUrl(filePath);
+    } catch (e) {
+      print("ERROR PHOTO UPLOAD: $e");
+      return null;
     }
   }
 
@@ -116,11 +160,12 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
                 padding: EdgeInsets.all(10),
                 child: ElevatedButton.icon(
                   style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF161616),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 25, vertical: 18)),
+                    backgroundColor: Color(0xFF161616),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 18),
+                  ),
                   onPressed: () {
                     setState(() {
                       _isFormVisible = !_isFormVisible;
@@ -135,65 +180,119 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
                     color: Colors.white,
                   ),
                 ),
-              )
+              ),
             ],
           ),
           AnimatedSize(
             duration: _animationDuration,
             curve: Curves.easeInOut,
-            child: _isFormVisible
-                ? Form(
-                    child: Container(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
+            child:
+                _isFormVisible
+                    ? Form(
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                SizedBox(
+                                  height: 120,
+                                  width: 120,
+                                  child:
+                                      pickedImage == null
+                                          ? GestureDetector(
+                                            onTap: handleImagePick,
+                                            child: Icon(
+                                              Icons.add_a_photo,
+                                              color: Colors.blue,
+                                              size: 50,
+                                            ),
+                                          )
+                                          : GestureDetector(
+                                            onTap: handleImagePick,
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(100),
+                                              child:
+                                                  pickedImage!.bytes != null
+                                                      ? Image.memory(
+                                                        Uint8List.fromList(
+                                                          pickedImage!.bytes!,
+                                                        ),
+                                                        fit: BoxFit.cover,
+                                                      )
+                                                      : Image.file(
+                                                        File(
+                                                          pickedImage!.path!,
+                                                        ),
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                            ),
+                                          ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
                                   child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: TextFieldStyle(
-                                    label: "Title",
-                                    inputController: _newsTitleController),
-                              )),
-                              Expanded(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextFieldStyle(
+                                      label: "Title",
+                                      inputController: _newsTitleController,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
                                   child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: TextFieldStyle(
-                                    label: "Author",
-                                    inputController: _newsAuthorController),
-                              ))
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextFieldStyle(
+                                      label: "Author",
+                                      inputController: _newsAuthorController,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
                                   child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: TextField(
-                                        controller: _newsContentController,
-                                        decoration: InputDecoration(
-                                            hintText: "Content",
-                                            enabledBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.grey)),
-                                            focusedBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.grey))),
-                                        maxLines: 5,
-                                      )))
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ElevatedButton(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextField(
+                                      controller: _newsContentController,
+                                      decoration: InputDecoration(
+                                        hintText: "Content",
+                                        enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ),
+                                      maxLines: 5,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
                                   style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.blue,
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 22, horizontal: 70),
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5))),
+                                    backgroundColor: Colors.blue,
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: 22,
+                                      horizontal: 70,
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
                                   onPressed: () {
                                     if (eid == 0) {
                                       insertNews();
@@ -204,14 +303,15 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
                                   child: Text(
                                     "Insert",
                                     style: TextStyle(color: Colors.white),
-                                  ))
-                            ],
-                          )
-                        ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                : Container(),
+                    )
+                    : Container(),
           ),
           Container(
             child: Center(
@@ -225,54 +325,56 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
             child: Padding(
               padding: EdgeInsets.all(8),
               child: DataTable(
-                  columns: [
-                    DataColumn(label: Text("Sl.No")),
-                    DataColumn(label: Text("Title")),
-                    DataColumn(label: Text("Content")),
-                    DataColumn(label: Text("Author")),
-/*                     DataColumn(label: Text("Status")),
+                columns: [
+                  DataColumn(label: Text("Sl.No")),
+                  DataColumn(label: Text("Title")),
+                  DataColumn(label: Text("Content")),
+                  DataColumn(label: Text("Author")),
+                  /*                     DataColumn(label: Text("Status")),
  */
-                    DataColumn(label: Text("Edit")),
-                    DataColumn(label: Text("Delete"))
-                  ],
-                  rows: newsList.asMap().entries.map((entry) {
-                    print(entry.value);
-                    return DataRow(cells: [
-                      DataCell(Text((entry.key).toString())),
-                      DataCell(Text(entry.value['newsletter_title'])),
-                      DataCell(Text(entry.value['newsletter_content'])),
-                      DataCell(Text(entry.value['newsletter_author'])),
-                      DataCell(IconButton(
-                        icon: Icon(
-                          Icons.edit,
-                          color: Colors.green,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _newsAuthorController.text =
-                                entry.value['newsletter_author'];
-                            _newsContentController.text =
-                                entry.value['newsletter_content'];
-                            _newsTitleController.text =
-                                entry.value['newsletter_title'];
-                            eid = entry.value['id'];
-                            _isFormVisible = !_isFormVisible;
-                          });
-                        },
-                      )),
-                      DataCell(IconButton(
-                        icon: Icon(
-                          Icons.delete,
-                          color: Colors.red,
-                        ),
-                        onPressed: () {
-                          delNews(entry.value['id'].toString());
-                        },
-                      ))
-                    ]);
-                  }).toList()),
+                  DataColumn(label: Text("Edit")),
+                  DataColumn(label: Text("Delete")),
+                ],
+                rows:
+                    newsList.asMap().entries.map((entry) {
+                      print(entry.value);
+                      return DataRow(
+                        cells: [
+                          DataCell(Text((entry.key).toString())),
+                          DataCell(Text(entry.value['newsletter_title'])),
+                          DataCell(Text(entry.value['newsletter_content'])),
+                          DataCell(Text(entry.value['newsletter_author'])),
+                          DataCell(
+                            IconButton(
+                              icon: Icon(Icons.edit, color: Colors.green),
+                              onPressed: () {
+                                setState(() {
+                                  _newsAuthorController.text =
+                                      entry.value['newsletter_author'];
+                                  _newsContentController.text =
+                                      entry.value['newsletter_content'];
+                                  _newsTitleController.text =
+                                      entry.value['newsletter_title'];
+                                  eid = entry.value['id'];
+                                  _isFormVisible = !_isFormVisible;
+                                });
+                              },
+                            ),
+                          ),
+                          DataCell(
+                            IconButton(
+                              icon: Icon(Icons.delete, color: Colors.red),
+                              onPressed: () {
+                                delNews(entry.value['id'].toString());
+                              },
+                            ),
+                          ),
+                        ],
+                      );
+                    }).toList(),
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
